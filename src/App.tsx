d14f8e3754ffa438fb40f975031c18252ec8e847
sdeevanapalli <s.deevanapalli@gmail.com>
2025-09-23 03:15:08 +0530

    added analytics

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Hero from "./components/Hero";
 import Projects from "./components/Projects";
 import About from "./components/About";
 import Contact from "./components/Contact";
+import { Analytics } from "@vercel/analytics/next";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <section id="projects"><Projects /></section>
       <section id="about"><About /></section>
       <section id="contact"><Contact /></section>
+      <Analytics />
     </div>
   );
 }
